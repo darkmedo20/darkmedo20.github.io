21fcedd0056ba4345755a806eef8ed7dcca2b448
darkmedo20 <ahmedalmostafaomer@gmail.com>
2025-09-21 14:38:31 +0300

    Ec2-4 HCI2

--- lms.js
+++ lms.js
@@ -19,6 +19,12 @@ const showMessage = (message, duration = 3000, isError = false) => {
                         podbeanUrl: `https://www.podbean.com/player-v2/?i=mu5mj-195813f-pb&from=pb6admin&share=0&download=0&rtl=0&fonts=Arial&skin=1b1b1b&font-color=ffffff&logo_link=episode_page&btn-skin=3267a3`,
                         quizUrl: `hciq1.html`
                     },
+                    {
+                        id: `lecture-2`,
+                        name: `Lecture 2: Human`,
+                        podbeanUrl: `https://www.podbean.com/player-v2/?i=mhak6-196f2d5-pb&from=pb6admin&share=0&download=0&rtl=0&fonts=Arial&skin=1b1b1b&font-color=auto&logo_link=episode_page&btn-skin=3267a3`,
+                        quizUrl: `hciq2.html`
+                    },
                     /*
                     ...Array.from({ length: 11 }, (_, i) => ({
                         id: `lecture-${i + 2}`,
@@ -36,6 +42,24 @@ const showMessage = (message, duration = 3000, isError = false) => {
                         name: `Lecture 1: Introduction to E-Commerce (1)`,
                         podbeanUrl: `https://www.podbean.com/player-v2/?i=fpjgu-19633f7-pb&from=pb6admin&share=0&download=0&rtl=0&fonts=Arial&skin=1b1b1b&font-color=ffffff&logo_link=episode_page&btn-skin=3267a3`,
                         quizUrl: `ecq1.html`
+                    },
+                    {
+                        id: `lecture-2`,
+                        name: `Lecture 2: Introduction to E-Commerce (2)`,
+                        podbeanUrl: `https://www.podbean.com/player-v2/?i=itriu-196f192-pb&from=pb6admin&share=0&download=0&rtl=0&fonts=Arial&skin=1b1b1b&font-color=ffffff&logo_link=episode_page&btn-skin=3267a3`,
+                        quizUrl: `ecq2.html`
+                    },
+                    {
+                        id: `lecture-3`,
+                        name: `Lecture 3: E-commerce Business Models and Concept (1)`,
+                        podbeanUrl: `https://www.podbean.com/player-v2/?i=wxbdr-196f233-pb&from=pb6admin&share=0&download=0&rtl=0&fonts=Arial&skin=1b1b1b&font-color=ffffff&logo_link=episode_page&btn-skin=3267a3`,
+                        quizUrl: `ecq3.html`
+                    },
+                    {
+                        id: `lecture-4`,
+                        name: `Lecture 4: E-commerce Business Models and Concept (2)`,
+                        podbeanUrl: `https://www.podbean.com/player-v2/?i=ptbie-196f249-pb&from=pb6admin&share=0&download=0&rtl=0&fonts=Arial&skin=1b1b1b&font-color=ffffff&logo_link=episode_page&btn-skin=3267a3`,
+                        quizUrl: `ecq4.html`
                     },
                     /*
                     ...Array.from({ length: 11 }, (_, i) => ({
