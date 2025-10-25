9fd89d2b5c82ed28d3e85600d0b553b3638af24c
darkmedo20 <ahmedalmostafaomer@gmail.com>
2025-10-25 11:37:57 +0300

    naq6

--- lms.js
+++ lms.js
@@ -191,12 +191,12 @@
                         podbeanUrl: `https://www.podbean.com/player-v2/?i=xj384-19a2484-pb&from=pb6admin&share=0&download=0&rtl=0&fonts=Arial&skin=1b1b1b&font-color=auto&logo_link=episode_page&btn-skin=3267a3`,
                         quizUrl: `naq5.html`
                     },
-                    /*{
+                    {
                         id: `lecture-6`,
-                        name: `Test`,
-                        podbeanUrl: `https://www.podbean.com/player-v2/?i=xj384-19a2484-pb&from=pb6admin&share=0&download=0&rtl=0&fonts=Arial&skin=1b1b1b&font-color=auto&logo_link=episode_page&btn-skin=3267a3`,
+                        name: `Lecture 6: System Backups`,
+                        podbeanUrl: `https://www.podbean.com/player-v2/?i=vwhie-19a30e3-pb&from=pb6admin&share=0&download=0&rtl=0&fonts=Arial&skin=1b1b1b&font-color=auto&logo_link=episode_page&btn-skin=3267a3`,
                         quizUrl: `naq6.html`
-                    },*/
+                    },
                     /*
                     ...Array.from({ length: 11 }, (_, i) => ({
                         id: `lecture-${i + 2}`,
